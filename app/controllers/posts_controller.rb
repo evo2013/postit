a51501869e1f9905b6cb60717909c8c6e03d81d4
evo2013 <CVMailer_2011@yahoo.com>
2015-10-04 12:19:22 -0700

    update current_user for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.creator = User.first #TODO: Change once we have authentication
+    @post.creator = current_user 
 
     if @post.save
       flash[:notice] = "Your post was created. "
